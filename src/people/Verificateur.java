31f848a4b3deac0d8984ba3465a5c22e7dc4ec9f
ophiran <ophiran@hotmail.com>
2013-03-21 13:21:25 +0100

    ajouter un enter

--- src/people/Verificateur.java
+++ src/people/Verificateur.java
@@ -4,4 +4,5 @@ package people;
 public abstract class Verificateur {
     public abstract boolean validation(String username,String password);
     public abstract boolean validation(Utilisateur utilisateur);
+    
 }
